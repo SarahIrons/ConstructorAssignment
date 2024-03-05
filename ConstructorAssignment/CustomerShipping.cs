d72b4b3d333e5d4d75182d8660793834e37b885b
SarahIrons <sarahirons11@gmail.com>
2024-03-05 15:50:09 -0500

    Add project files.

---
+++ ConstructorAssignment/CustomerShipping.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorAssignment
+{
+    public class CustomerShipping
+    
+    { 
+        public const int WarehouseNumber= 459; //used const as the location of warehouse where items are shipped from because const variable cannot be changed. 
+        public string ShippingStatus;//this variable gives info to the customer about status of shipment (recieved/delivered/etc)
+
+        public CustomerShipping() : this("Your order was received and we are preparing shipment!")
+        {
+            
+        }
+
+        public CustomerShipping(string ShippingStatus)
+        {//below I used the var for the update message so that it could be used in different places n(ex, email/text possibly) and to make it easier to read.
+            var customerUpdateMessage = "Shipping from Warehouse Number: {0} Shipping Status: {1}";
+            this.ShippingStatus = ShippingStatus; //this code passes through info into the message to the customer. this is part of chaining the constructor.
+           Console.WriteLine(customerUpdateMessage, WarehouseNumber, ShippingStatus);
+
+        }
+    }
+}
---
+++ ConstructorAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+//CONSTRUCTOR ASSIGNMENT
+
+//Perform these actions and create a console app that includes the following:
+
+
+//Create a const variable.
+
+//Create a variable using the keyword “var.”
+
+//Chain two constructors together.
+
+//Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main()
+        { 
+            CustomerShipping customerShipping = new CustomerShipping();
+            CustomerShipping customerShipping1 = new CustomerShipping("Shipment delivered!");
+        }
+        
+    }
+}
